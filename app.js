6d0029c45b19ab53621ebcb008e163a015f11d26
Andrew Nicholls <apnicholls56@yahoo.com>
2021-09-13 16:28:21 -0500

    checking in

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
+var passport = require('passport');
 var logger = require('morgan');
 require('./lib/seed');
 require('./lib/models');
@@ -10,6 +11,8 @@ var cors = require('cors');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var apiRouter = require('./routes/api');
+
+require('./lib/auth');
 
 var app = express();
 app.use(cors());
